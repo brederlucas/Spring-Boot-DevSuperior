768ab90a33f3be78e46b955f8783c76648a04beb
Lucas Breder <lucas_breder_andrade@hotmail.com>
2025-01-21 23:14:46 -0300

    GmaeMinDTO, GameRepository, GameService, GameController

---
+++ dslist/src/main/java/com/devesuperior/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.devesuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devesuperior.dslist.dto.GameMinDTO;
+import com.devesuperior.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameservice;
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameservice.findAll();
+        return result;
+    }
+}
---
+++ dslist/src/main/java/com/devesuperior/dslist/services/GameService.java
@@ -0,0 +1,26 @@
+package com.devesuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devesuperior.dslist.dto.GameMinDTO;
+import com.devesuperior.dslist.entities.Game;
+import com.devesuperior.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    //Métodos
+
+    // Função que retorna uma lista de GameMinDTO
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+        
+    }
+}
