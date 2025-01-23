3d6e2f2c1a6e888021bbd2aac0de26f3b71a7856
Lucas Breder <lucas_breder_andrade@hotmail.com>
2025-01-22 23:52:12 -0300

    Get game by id

--- dslist/src/main/java/com/devesuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devesuperior/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.devesuperior.dslist.dto.GameDTO;
 import com.devesuperior.dslist.dto.GameMinDTO;
 import com.devesuperior.dslist.entities.Game;
 import com.devesuperior.dslist.repositories.GameRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class GameService {
@@ -17,10 +21,17 @@ public class GameService {
 
     //Métodos
 
+	@Transactional
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
+	}
+
     // Função que retorna uma lista de GameMinDTO
-    public List<GameMinDTO> findAll(){
-        List<Game> result = gameRepository.findAll();
-        return result.stream().map(x -> new GameMinDTO(x)).toList();
-        
-    }
+	@Transactional
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(GameMinDTO::new).toList();
+	}
+
 }
